90ded4760e43937f2c37b0cf2bc90772ed63e0bf
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-09-25 17:43:30 -0700

    made markets display loop instead of market display

--- demos/Angular-Redux/src/app/market-display/market-display.component.ts
+++ demos/Angular-Redux/src/app/market-display/market-display.component.ts
@@ -5,12 +5,16 @@ import { Component, OnInit, Input } from '@angular/core';
   templateUrl: './market-display.component.html',
   styleUrls: ['./market-display.component.css']
 })
+
 export class MarketDisplayComponent implements OnInit {
 
-  @Input('marketsList') marketsList: Array<Object>;
+  @Input('marketId') marketId: number;
+  @Input('location') location: string;
+  @Input('cards') cards: number;
   @Input('totalCards') totalCards: number;
   @Input('addCard') addCard: Function;
   @Input('deleteCard') deleteCard: Function;
+  @Input('index') index: number;
 
   constructor() { 
     this.getPercent = this.getPercent.bind(this);
